96af3f3be5701835925ff3437b907aaeebe666f6
Scott Hoisington <hoyzington@gmail.com>
2020-06-25 16:18:43 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,30 @@
 Rails.application.routes.draw do
-  resources :student_statuses
-  resources :homeworks
-  resources :klasses
-  resources :teachers
-  resources :students
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'pages#home'
+  get 'home', to: 'pages#home'
+
+  get 'teachers/signup', to: 'teachers#new'
+  resources :teachers, do
+    get 'classes/new', to: 'klasses#new'
+    get 'classes/:id/edit', to: 'klasses#edit'
+    get 'classes', to: 'klasses#index'
+    get 'classes/:id', to: 'klasses#show'
+    resources :klasses
+  end
+
+  get 'classes/:id', to: 'klasses#show' do
+    resources :student_statuses
+    get 'homework/new', to: 'homeworks#new'
+    get 'homework/:id/edit', to: 'homeworks#edit'
+    get 'homework', to: 'homeworks#index'
+    get 'homework/:id', to: 'homeworks#show'
+    resources :homeworks
+  end
+  
+  get 'students/signup', to: 'students#new'
+  resources :students do
+    get 'student_status/:id', to: 'student_statuses#show'
+    get 'homework', to: 'homeworks#index'
+  end
+
 end
