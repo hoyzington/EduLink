97dd30a2b22dc9b1790d709224d7edeec62f8b8f
Scott Hoisington <hoyzington@gmail.com>
2020-07-23 17:16:30 -0400

    change @student to @student_status

--- app/controllers/student_statuses_controller.rb
+++ app/controllers/student_statuses_controller.rb
@@ -1,6 +1,6 @@
 class StudentStatusesController < ApplicationController
 
-  before_action :set_student, except: [:new, :create, :index]
+  before_action :set_student_status, except: [:new, :create, :index]
   before_action :set_klass, except: [:create, :edit]
 
   def new
@@ -8,10 +8,7 @@ class StudentStatusesController < ApplicationController
   end
 
   def create
-    # byebug
     @student_status = StudentStatus.new(status_params)
-    student = Student.find_by(id_number: @student_status.id_number)
-    @student_status.student = student if student
     if @student_status.save
       flash[:notice] = "#{@student_status.full_name} was added successfully."
       redirect_to klass_student_statuses_new_path(@student_status.klass)
@@ -24,35 +21,35 @@ class StudentStatusesController < ApplicationController
   end
 
   def update
-    if @student.update(status_params)
-      flash[:notice] = "#{@student.full_name}'s profile was successfully updated."
-      redirect_to klass_student_status_path(@klass, @student)
+    if @student_status.update(status_params)
+      flash[:notice] = "#{@student_status.full_name}'s profile was successfully updated."
+      redirect_to klass_student_status_path(@klass, @student_status)
     else
       render 'edit'
     end
   end
 
   def index
-    @students = @klass.student_statuses.select {|ss| ss.id_number > 0}
+    @student_statuses = @klass.student_statuses.select {|ss| ss.id_number > 0}
   end
 
   def show
   end
 
   def destroy
-    @student.destroy unless @student.id_number == 0
+    @student_status.destroy unless @student_status.id_number == 0
     flash[:notice] = 'The student has been removed from this class.'
     redirect_to klass_students_path(@klass)
   end
 
   private
 
-  def set_student
-    @student = StudentStatus.find(params[:id])
+  def set_student_status
+    @student_status = StudentStatus.find(params[:id])
   end
 
   def set_klass
-    @klass = Klass.find(params[:class_id] || params[:klass_id] || @student.klass_id)
+    @klass = Klass.find(params[:class_id] || params[:klass_id] || @student_status.klass_id)
   end
 
   def status_params
