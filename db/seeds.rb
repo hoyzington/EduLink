e62974d05e3715e8c053611f6706721ae042cb3c
Scott Hoisington <hoyzington@gmail.com>
2020-08-05 21:39:49 -0400

    improve html; initial work on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Teachers
+
+Teacher.create(id_number: 0, first_name: 'William', last_name: 'McKinley', dept: 'Admin', email: 'william@email.com', password: 'password')
+
+hector = Teacher.create(id_number: 100, first_name: 'Hector', last_name: 'LaCovara', dept: 'Science', email: 'hector@email.com', password: 'password')
+
+leslie = Teacher.create(id_number: 101, first_name: 'Leslie', last_name: 'Foote', dept: 'Social Studies', email: 'leslie@email.com', password: 'password')
+
+frank = Teacher.create(id_number: 102, first_name: 'Frank', last_name: 'Kowchevski', dept: 'Math', email: 'frank@email.com', password: 'password')
+
+## Klasses
+
+klasses = []
+
+klasses << hector.klasses.create(name: 'Chemistry', period: '1', dept: hector.dept)
+
+klasses << leslie.klasses.create([{name: 'American History 1', period: '3', dept: leslie.dept}, {name: 'AP Government', period: '6', dept: leslie.dept}])
+
+klasses << frank.klasses.create([{name: 'Geometry', period: '2', dept: frank.dept}, {name: 'Calculus', period: '5', dept: frank.dept}])
+
+klasses = klasses.flatten
+
+## Students
+
+students = Student.create([{id_number: 0, first_name: 'Default', last_name: 'Student', email: 'default@email.com', password: 'password'}, {id_number: 1000, first_name: 'Lindsay', last_name: 'Weir', email: 'lindsay@email.com', password: 'password'}, {id_number: 1001, first_name: 'Daniel', last_name: 'Desario', email: 'daniel@email.com', password: 'password'}, {id_number: 1002, first_name: 'Nick', last_name: 'Andopolis', email: 'nick@email.com', password: 'password'}, {id_number: 1003, first_name: 'Ken', last_name: 'Miller', email: 'ken@email.com', password: 'password'}])
+
+## StudentStatuses
+
+def ss_data(students)
+  ss_array = []
+  students.each do |s|
+    ss_array << {id_number: s.id_number, first_name: s.first_name, last_name: s.last_name, student_id: s.id}
+  end
+  ss_array
+end
+
+klasses.each do |klass|
+  klass.student_statuses.create(ss_data(students))
+end
+
+klasses.each do |klass|
+  klass.student_statuses.create(id_number: 1004, first_name: 'Kim', last_name: 'Kelly', student_id: 0)
+end
+
+## Homeworks
+
+def h_data(students)
+  h_array = []
+  students.each do |h|
+    # h_array << {date: , read: "Chapter #{num}", exercises: "All from chapter #{num}", other: '', notes: '', student_id: h.id}
+  end
+  h_array
+end
+
+klasses.each do |klass|
+  klass.homeworks.create(h_data(students))
+end
+
+Homework.create([{date: , read: '', exercises: '', other: '', notes: '', done: , klass: klasses[], student: students[]}, {date: , read: '', exercises: '', other: '', notes: '', done: , klass: klasses[], student: students[]}])
