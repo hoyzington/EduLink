8ddab3b99efdd516915bef58c2a981ace014a99b
Scott Hoisington <hoyzington@gmail.com>
2020-11-22 18:54:36 -0500

    organize methods & add comment

--- app/helpers/homeworks_helper.rb
+++ app/helpers/homeworks_helper.rb
@@ -2,6 +2,12 @@ module HomeworksHelper
 
   FIRST_ID = 1
 
+# Helper methods for displaying/formatting homework radio button text
+
+  def add_day
+    60*60*24
+  end
+
   def future_weekday_only
     if Time.now.friday?
       num = 3
@@ -13,26 +19,17 @@ module HomeworksHelper
     Time.now + (add_day * num)
   end
 
-  def add_day
-    60*60*24
-  end
-
   def homework_assigned?
     !!@klass.homeworks.detect{|h| h.formatted_date == @date.strftime(day_format)}
-  end
-
-  def homework_for(date)
-    homeworks = @klass.homeworks.select {|h| h.is_default}
-    homeworks.detect {|h| h.date.strftime("%y%m%d") == date.strftime("%y%m%d")}
-  end
-
-  def next_day
-    @date.friday? ? @date += (add_day * 3) : @date += add_day
   end
 
   def clickable_homework_to_edit
     title = "✔ #{@date.strftime(day_format)}"
     link_to title, klass_homeworks_edit_path(@klass, homework_for(@date))
+  end
+
+  def next_day
+    @date.friday? ? @date += (add_day * 3) : @date += add_day
   end
 
 end
